89eb8a2c3236af78683721d9300a18b218a3f692
Maya Angelou <MayaAngelou@poets.com>
2018-06-13 15:10:44 -0700

    working signin.js

--- models/schedule.js
+++ models/schedule.js
@@ -1,9 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
     var Schedule = sequelize.define("Schedule", {
-      tripID: {
-        type: DataTypes.INT,
-        allowNull: false
-      },
       title: {
         type: DataTypes.STRING,
         allowNull: false,
