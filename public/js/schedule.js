9e3c4c1ee76f47990305278eaf0fb93dd3d6963d
basosa94 <basosa@ucdavis.edu>
2018-06-14 12:00:58 -0700

    edit schedule.js

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,25 +1,102 @@
-$(document).ready(function(){
-    // var user = localStorage.currentUser;
-    // var trip = localStorage.currentTrip;
+$("#submitBtn").on("click", function(event) {
 
-    $("#submitBtn").on("click", function(event) {
-        event.preventDefault();
+    event.preventDefault();
 
-        var schedItem = {
-            // tripID: trip,
-            title: $("#title").val().trim(),
-            body: $("#body").val().trim(),
-            startTime: $("#start").val().trim(),
-            endTime: $("#end").val().trim()
+    var list=[];
+
+    getList();
+
+    function initializeList() {
+        $(".todo-list").empty();
+        var rowsToAdd=[];
+        for (var i=0; i < list.length; i++) {
+            rowsToAdd.push(createNewRow(list[i]));
         }
+        $(".todo-list").prepend(rowsToAdd);
+    }
 
-
-        $.post("/api/schedule", schedItem).then(function(response) {
-            // console.log(response);
-            var newLine = $(`<li class="list-group-item new-item form-color">${response.body}</li>`)
-            $(".todo-list").append(newLine);
+    function getList() {
+        $.get("/api/schedule", function(data) {
+            list = data;
+            initializeList();
         });
+    }
+
+    var schedItem = {
+        title: $("#title").val().trim(),
+        body: $("#body").val().trim(),
+        startTime: $("#start").val().trim(),
+        endTime: $("#end").val().trim()
+    }
+
+    $('#title').val('')
+    $('#body').val('')
+    $("#start").val('')
+    $("#end").val('')
+
+    $.post("/api/schedule", schedItem).then(function(response) {
+        // console.log(response);
+        var newLine = $(`<li class="list-group-item new-item form-color appendBtn">${response.title}</li>`)
+        var deleteBtn = $("<button>");
+        deleteBtn.text("X");
+        deleteBtn.addClass("delete btn btn-danger");
+        var editBtn = $("<button>");
+        editBtn.text("EDIT");
+        editBtn.addClass("edit btn btn-info");
+        $(".todo-list").append(newLine);
+        $(newLine).append(deleteBtn);
+        $(newLine).append(editBtn);
+    });
+
+    $(".delete").on("click", function(event){
+        event.stopPropagation();
+        console.log($(this).data)
+        var id = $(this).data("id");
+        $.ajax({
+            method: "DELETE",
+            url: "/api/schedule/:id" 
+    }).then(getList());
+    });
+          // $(document).on("click", "button.delete", deleteTodo);
+    // $(document).on("click", "button.complete", toggleComplete);
+    // $(document).on("click", ".todo-item", editTodo);
+    // $(document).on("keyup", ".todo-item", finishEdit);
+    // $(document).on("blur", ".todo-item", cancelEdit);
+    // $(document).on("submit", "#todo-form", insertTodo);
+
+    // var list=[];
+
+    // getList();
+
+    // function initializeList() {
+    //     $(".todo-list").empty();
+    //     var rowsToAdd=[];
+    //     for (var i=0; i < list.length; i++) {
+    //         rowsToAdd.push(createNewRow(list[i]));
+    //     }
+    //     $(".todo-list").prepend(rowsToAdd);
+    // }
+
+    // function getList() {
+    //     $.get("/api/schedule", function(data) {
+    //         list = data;
+    //         initializeList();
+    //     });
+    // }
+
+        // function updateSchedule() {
+        //     $.ajax({
+        //         method: "PUT",
+        //         url: "/api/schedule",
+        //         data: todo
+        //     }).then(getSchedule);
+        // }
+        // $.get()
+
+        // $.put("/api/schedule", schedItem).then(function)
+
+        // $.delete()
 
      
-    });
 });
+
